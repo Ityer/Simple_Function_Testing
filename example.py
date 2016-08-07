43d47ca06e2d7513cb2e542298d9e0d8c5b8dd28
Ityer <andrewmackay1996@gmail.com>
2016-08-07 22:19:08 +0100

    keeps example open at end

--- example.py
+++ example.py
@@ -9,3 +9,5 @@ black.box("TimePass1",TimesPass(10,10),100) # 10*10 should == 100
 
 black.box("TimeFail1",TimesFail(2,2),4) # 2*2 should == 4 (False Negative)
 black.box("TimesFail2",TimesFail(10,10),100)
+print()
+input("Press Ender To Close")
