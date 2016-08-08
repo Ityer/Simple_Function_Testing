19086154ac06098588e7033dba29f2a925aa2ef3
Ityer <andrewmackay1996@gmail.com>
2016-08-08 21:51:38 +0100

    test for function that will return a range of numbers

--- black.py
+++ black.py
@@ -6,3 +6,10 @@ def box(n,x,a): # Test Name, Function(variable inputs), What should be returned
 		print("%s: Failed"% n)
 		return False
 
+def boxIntRange(n,min,max,a):# Test Name, Minimim(inclusive), Maximim(inclusive), actual
+	if (a >= min) and (a <= max):
+		print("%s: Passed"% n)
+		return True
+	else:
+		print("%s: Failed"% n)
+		return False
--- example.py
+++ example.py
@@ -1,13 +1,18 @@
-import black
+import black, random
 def TimesPass(a,b):
-        return a*b
+	return a*b
 
 def TimesFail(a,b):
-        return a+b
-	
+	return a+b
+
+def RandomOneToTen():
+	return random.randint(min,max)
+
 black.box("TimePass1",TimesPass(10,10),100) # 10*10 should == 100
 
 black.box("TimeFail1",TimesFail(2,2),4) # 2*2 should == 4 (False Negative)
 black.box("TimesFail2",TimesFail(10,10),100)
 print()
+black.boxIntRange("RandomOneToTenPass",1,10,random.randint(1,10))
+black.boxIntRange("RandomOneToTenFail1",1,10,random.randint(1,11)) #will mostly return as passed
 input("Press Ender To Close")
