2d0978b551322f809e84231a71cdff79328a3d20
Ityer <andrewmackay1996@gmail.com>
2017-08-30 19:06:59 +0100

    comments. fixes. new test. Functionised pass fail
    comments. New test that takes a list of possible outcomes. Removal of
    repeated pass and fail prints to make it eaiser to change all at once.
    Fails now give information as to why the test failed.

--- black.py
+++ black.py
@@ -1,15 +1,26 @@
-def box(n,x,a): # Test Name, Function(variable inputs), What should be returned
-	if x == a:
-		print("%s: Passed"% n)
+def box(n,x,a): #Test Name, Function(variable inputs), What should be returned
+	if x == a: #if returned is as expected
 		return True
 	else:
-		print("%s: Failed"% n)
+		f(n,x,a)#print fail
 		return False
 
-def boxIntRange(n,min,max,a):# Test Name, Minimim(inclusive), Maximim(inclusive), actual
-	if (a >= min) and (a <= max):
-		print("%s: Passed"% n)
+def boxIntRange(n,min,max,x):# Test Name, Minimim(inclusive), Maximim(inclusive), actual
+	if (x >= min) and (x <= max):#if returned within range expected
+		print("%s: Passed"% n)#print pass
 		return True
 	else:
-		print("%s: Failed"% n)
+		f(n,x,("Between %s and %s"%(min,max)))#print fail
 		return False
+
+def boxM(n,x,a): #Test Name, Function(variable inputs), What should be returned
+	if x in a: #if returned is as expected
+		print("%s: Passed"% n)#print pass
+		return True
+	else:
+		f(n,x,a)#print fail
+		return False
+def p(n):
+        print("%s: Passed"% n)#print pass
+def f(n,x,a):
+        print("%s: Failed - expected: \"%s\", recived : \"%s\""% (n,a,x))
--- example.py
+++ example.py
@@ -1,18 +1,22 @@
 import black, random
-def TimesPass(a,b):
+def TimesPass(a,b):#multiply two inputs and return result - functional
 	return a*b
 
-def TimesFail(a,b):
+def TimesFail(a,b):#multiply two inputs and return result - non-functional
 	return a+b
 
-def RandomOneToTen():
+def RandomOneToTen(min,max):#random number generator
 	return random.randint(min,max)
 
 black.box("TimePass1",TimesPass(10,10),100) # 10*10 should == 100
 
-black.box("TimeFail1",TimesFail(2,2),4) # 2*2 should == 4 (False Negative)
+black.box("TimeFail1",TimesFail(2,2),4) # 2*2 should == 4 (False possitive)
 black.box("TimesFail2",TimesFail(10,10),100)
 print()
 black.boxIntRange("RandomOneToTenPass",1,10,random.randint(1,10))
-black.boxIntRange("RandomOneToTenFail1",1,10,random.randint(1,11)) #will mostly return as passed
-input("Press Ender To Close")
+for i in range(10):
+        black.boxIntRange("RandomOneToTenFail1",1,10,random.randint(1,15)) #will mostly return as passed
+
+black.boxM("Mpass",5,[1,2,3,4,5])
+black.boxM("Mfail",99,[1,2,3,4,5])
+input("Press Enter To Close")
