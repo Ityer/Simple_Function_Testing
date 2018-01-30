5b7c55cd0dc2c8b7d89c99896e769681055ae90e
Ityer <andrewmackay1996@gmail.com>
2018-01-30 17:21:55 +0000

    Simplified Functions
    removed if statements. Replaceed with return statment with boolean

--- black.py
+++ black.py
@@ -1,25 +1,11 @@
 def box(n,x,a): #Test Name, Function(variable inputs), What should be returned
-	if x == a: #if returned is as expected
-		return True
-	else:
-		f(n,x,a)#print fail
-		return False
+        return (x==a)
 
 def boxIntRange(n,min,max,x):# Test Name, Minimim(inclusive), Maximim(inclusive), actual
-	if (x >= min) and (x <= max):#if returned within range expected
-		print("%s: Passed"% n)#print pass
-		return True
-	else:
-		f(n,x,("Between %s and %s"%(min,max)))#print fail
-		return False
+	return ((x >= min) and (x <= max))#if returned within range expected
 
 def boxM(n,x,a): #Test Name, Function(variable inputs), What should be returned
-	if x in a: #if returned is as expected
-		print("%s: Passed"% n)#print pass
-		return True
-	else:
-		f(n,x,a)#print fail
-		return False
+	return (x in a)
 def p(n):
         print("%s: Passed"% n)#print pass
 def f(n,x,a):
--- example.py
+++ example.py
@@ -8,15 +8,14 @@ def TimesFail(a,b):#multiply two inputs and return result - non-functional
 def RandomOneToTen(min,max):#random number generator
 	return random.randint(min,max)
 
-black.box("TimePass1",TimesPass(10,10),100) # 10*10 should == 100
-
-black.box("TimeFail1",TimesFail(2,2),4) # 2*2 should == 4 (False possitive)
-black.box("TimesFail2",TimesFail(10,10),100)
+print(black.box("TimePass1",TimesPass(10,10),100)) # 10*10 should == 100
+print(black.box("TimesFail2",TimesFail(10,10),100))
 print()
-black.boxIntRange("RandomOneToTenPass",1,10,random.randint(1,10))
+print(black.boxIntRange("RandomOneToTenPass",1,10,random.randint(1,10)))
 for i in range(10):
-        black.boxIntRange("RandomOneToTenFail1",1,10,random.randint(1,15)) #will mostly return as passed
-
-black.boxM("Mpass",5,[1,2,3,4,5])
-black.boxM("Mfail",99,[1,2,3,4,5])
+        print(black.boxIntRange("RandomOneToTenFail1",1,10,random.randint(1,15))) #will mostly return as passed
+print()
+print(black.boxM("Mpass",5,[1,2,3,4,5]))
+print(black.boxM("Mfail",99,[1,2,3,4,5]))
+print()
 input("Press Enter To Close")
